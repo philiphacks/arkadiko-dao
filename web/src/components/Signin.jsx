29ca9f1fcc52fd3a15ef3f01d97d26c3bb65318a
Philip De Smedt <philip.desmedt@gmail.com>
2021-02-28 10:56:13 +0100

    Add copy

--- web/src/components/Signin.jsx
+++ web/src/components/Signin.jsx
@@ -7,7 +7,7 @@ export const Signin = () => {
     <Box width="100%" textAlign="center">
       <Box maxWidth="800px" mx="auto" mt={[6, '100px']}>
         <Text fontWeight="700" fontSize={['36px', '50px']} lineHeight={1} display="block">
-          ArkDAO - Stablecoin Liquidity on Stacks
+          ArkDAO - Stablecoin Liquidity on Stacks and Bitcoin
         </Text>
         <Box mt={[5, '60px']}>
           <Button onClick={() => authenticate()}>Get Started</Button>
