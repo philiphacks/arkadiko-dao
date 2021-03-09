8ff05e83dec5fa3b819f75d4bc6d7c59ddef7237
Philip De Smedt <philip.desmedt@gmail.com>
2021-03-09 18:11:36 +0100

    Show dynamic risk parameters on vaults

--- web/packages/web-app/components/vault.tsx
+++ web/packages/web-app/components/vault.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { getCollateralToDebtRatio } from '@common/get-collateral-to-debt-ratio';
 import { NavLink as RouterLink } from 'react-router-dom'
+import { AppContext } from '@common/context';
 
 interface VaultProps {
   id: string;
@@ -9,10 +10,23 @@ interface VaultProps {
 }
 
 export const Vault: React.FC<VaultProps> = ({ id, stxCollateral, coinsMinted }) => {
-  let debtRatio = {};
+  const state = useContext(AppContext);
+  let debtRatio = 0;
   if (id) {
-    debtRatio = getCollateralToDebtRatio(id);
+    debtRatio = getCollateralToDebtRatio(id)?.collateralToDebt;
   }
+
+  const debtClass = () => {
+    if (debtRatio >= 200) {
+      return 'text-green-400';
+    } else if (debtRatio >= 180) {
+      return 'text-orange-400';
+    } else if (debtRatio > 160) {
+      return 'text-red-400';
+    }
+
+    return 'text-red-400';
+  };
 
   return (
     <tr className="bg-white">
@@ -24,13 +38,13 @@ export const Vault: React.FC<VaultProps> = ({ id, stxCollateral, coinsMinted }) 
         </span>
       </td>
       <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
-        <span className="text-gray-900 font-medium">0.0%</span>
+        <span className="text-gray-900 font-medium">{state.riskParameters['stability-fee']}%</span>
       </td>
       <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
-        <span className="text-gray-900 font-medium">150%</span>
+        <span className="text-gray-900 font-medium">{state.riskParameters['liquidation-ratio']}%</span>
       </td>
       <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
-        <span className="text-gray-900 font-medium">TBD</span>
+        <span className={`${debtClass()} font-medium`}>{debtRatio}%</span>
       </td>
       <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
         <span className="text-gray-900 font-medium">{coinsMinted / 1000000}</span>
