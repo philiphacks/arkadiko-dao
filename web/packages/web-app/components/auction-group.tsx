e500ab8f21d80dbab06ad5f222300c5aaf9bb8f4
Philip De Smedt <philip.desmedt@gmail.com>
2021-03-12 15:50:07 +0100

    Add additional columns for auctions

--- web/packages/web-app/components/auction-group.tsx
+++ web/packages/web-app/components/auction-group.tsx
@@ -34,6 +34,15 @@ export const AuctionGroup: React.FC<AuctionProps[]> = ({ auctions }) => {
                   Current $ Price/STX
                 </th>
                 <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Minimum Bid
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Current Bid
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Ends at (block height)
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 </th>
               </tr>
             </thead>
--- web/packages/web-app/components/auction.tsx
+++ web/packages/web-app/components/auction.tsx
@@ -17,6 +17,15 @@ export const Auction: React.FC<AuctionProps> = ({ id, ustxAmount, price }) => {
         <span className="text-gray-900 font-medium">${price / 100}</span>
       </td>
       <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
+        <span className="text-gray-900 font-medium">${price / 100}</span>
+      </td>
+      <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
+        <span className="text-gray-900 font-medium">TBD</span>
+      </td>
+      <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
+        <span className="text-gray-900 font-medium">TBD</span>
+      </td>
+      <td className="px-6 py-4 text-left whitespace-nowrap text-sm text-gray-500">
         <span className="text-gray-900 font-medium">
           <RouterLink to={`vaults/${id}`} exact className="px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Bid
