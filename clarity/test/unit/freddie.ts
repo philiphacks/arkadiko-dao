e52fc79c570d7774f294a0b5324bb37206ccfdd3
Philip De Smedt <philip.desmedt@gmail.com>
2021-03-13 11:45:38 +0100

    Add Freddie - a vault manager that acts as a communication layer to talk to collateral reserves

--- clarity/test/unit/freddie.ts
+++ clarity/test/unit/freddie.ts
@@ -1,10 +1,11 @@
 import { Client, Provider, ProviderRegistry } from "@blockstack/clarity";
 
-describe("stacks reserve test suite", () => {
+describe("freddie test suite", () => {
   let vaultTrait: Client;
   let stxReserveClient: Client;
   let oracleClient: Client;
   let tokenClient: Client;
+  let freddieClient: Client;
   let provider: Provider;
 
   before(async () => {
@@ -13,13 +14,15 @@ describe("stacks reserve test suite", () => {
     oracleClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.oracle", "oracle", provider);
     tokenClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.xusd-token", "xusd-token", provider);
     stxReserveClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.stx-reserve", "stx-reserve", provider);
+    freddieClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.freddie", "freddie", provider);
   });
 
   it("should have a valid syntax", async () => {
     await vaultTrait.deployContract();
     await tokenClient.deployContract();
     await oracleClient.deployContract();
-    await stxReserveClient.checkContract();
+    await stxReserveClient.deployContract();
+    await freddieClient.checkContract();
   });
 
   after(async () => {
