90727c2c103b6eea476457a95062529cd5e4258a
Philip De Smedt <philip.desmedt@gmail.com>
2021-03-23 21:54:41 +0100

    Fix tests

--- clarity/tests/clarinet_freddie_test.ts
+++ clarity/tests/clarinet_freddie_test.ts
@@ -2,26 +2,12 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 Clarinet.test({
-  name: "Ensure that counter can be incremented multiples per block, accross multiple blocks",
+  name: "Returns the correct name of the Arkadiko Token",
   async fn(chain: Chain, accounts: Array<Account>) {
     let block = chain.mineBlock([
       Tx.contractCall("arkadiko-token", "name", [], accounts[0].address)
     ]);
     assertEquals(block.height, 2);
-    // assertEquals(block.receipts[0].result, "(ok u2)");
-    // assertEquals(block.receipts[1].result, "(ok u6)");
-    // assertEquals(block.receipts[2].result, "(ok u16)");
-    console.log(block, 'blaat');
-
-    // block = chain.mineBlock([
-    //   Tx.contractCall("counter", "increment", [types.uint(1)], accounts[0].address),
-    //   Tx.contractCall("counter", "increment", [types.uint(4)], accounts[0].address),
-    //   Tx.contractCall("counter", "increment", [types.uint(10)], accounts[0].address)
-    // ]);
-    // assertEquals(block.height, 3);
-    // assertEquals(block.receipts[0].result, "(ok u17)");
-    // assertEquals(block.receipts[1].result, "(ok u21)");
-    // assertEquals(block.receipts[2].result, "(ok u31)");
-    // console.log(block);
+    assertEquals(block.receipts[0].result, '(ok "Arkadiko")');
   }
 });
