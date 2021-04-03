37ef6b167b82cf8412099b965b32a03044936651
Philip De Smedt <philip.desmedt@gmail.com>
2021-04-03 20:37:01 +0200

    cleanup dependencies

--- web/packages/web-app/common/get-balance.ts
+++ web/packages/web-app/common/get-balance.ts
@@ -1,5 +1,20 @@
 import { getRPCClient } from './utils';
 
+// const fetchBalances = async (principal: string) => {
+//   const url = `${this.url}/extended/v1/address/${principal}/balances`;
+//   const response = await fetch(url, { credentials: 'omit' });
+//   const data = await response.json();
+//   // console.log(data);
+//   const dikoBalance = data.fungible_tokens['ST31HHVBKYCYQQJ5AQ25ZHA6W2A548ZADDQ6S16GP.arkadiko-token::diko'];
+//   const xusdBalance = data.fungible_tokens['ST31HHVBKYCYQQJ5AQ25ZHA6W2A548ZADDQ6S16GP.xusd-token::xusd'];
+//   return {
+//     stx: data.stx.balance,
+//     xusd: xusdBalance ? xusdBalance.balance : 0,
+//     diko: dikoBalance ? dikoBalance.balance : 0,
+//     nonce: data.nonce,
+//   };
+// }
+
 export const fetchBalances = async (stxAddress: string) => {
   const client = getRPCClient();
 
