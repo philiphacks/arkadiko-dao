ed2a1090a39b29154fbca6238c23c010bfe6163f
Philip De Smedt <philip.desmedt@gmail.com>
2021-04-14 18:51:14 +0200

    add changelog

--- web/components/auction.tsx
+++ web/components/auction.tsx
@@ -120,10 +120,6 @@ export const Auction: React.FC<AuctionProps> = ({ id, lotId, collateralToken, de
               <button type="button" onClick={() => setBidParams()} className="mr-2 px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Bid
               </button>
-
-              <button type="button" onClick={() => setBidParams()} className="px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Buy lot
-              </button>
             </div>
           )}
         </span>
