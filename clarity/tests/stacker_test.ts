896ccbc5b561df9a60553ab3ef78fb1b5ed4addc
Philip De Smedt <philip.desmedt@gmail.com>
2021-04-28 10:29:34 +0200

    add test for payout of multiple vaults

--- clarity/tests/stacker_test.ts
+++ clarity/tests/stacker_test.ts
@@ -73,7 +73,7 @@ Clarinet.test({
 });
 
 Clarinet.test({
-  name: "stacker: payout vaults",
+  name: "stacker: payout one vault",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     let deployer = accounts.get("deployer")!;
     let block = chain.mineBlock([
@@ -125,5 +125,82 @@ Clarinet.test({
     let vault = call.result.expectTuple();
     vault['stacked-tokens'].expectUint(2000000000);
     vault['collateral'].expectUint(2000000000);
+  }
+});
+
+Clarinet.test({
+  name: "stacker: payout multiple vaults",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get("wallet_1")!;
+    let deployer = accounts.get("deployer")!;
+    let block = chain.mineBlock([
+      Tx.contractCall("oracle", "update-price", [
+        types.ascii("STX"),
+        types.uint(200),
+      ], deployer.address),
+      Tx.contractCall("freddie", "collateralize-and-mint", [
+        types.uint(1000000000),
+        types.uint(1000000000), // mint 1000 xUSD
+        types.principal(deployer.address),
+        types.ascii("STX-A"),
+        types.ascii("STX"),
+        types.principal("STSTW15D618BSZQB85R058DS46THH86YQQY6XCB7.stx-reserve"),
+        types.principal(
+          "STSTW15D618BSZQB85R058DS46THH86YQQY6XCB7.arkadiko-token",
+        ),
+      ], deployer.address),
+      Tx.contractCall("freddie", "collateralize-and-mint", [
+        types.uint(500000000),
+        types.uint(400000000), // mint 400 xUSD
+        types.principal(wallet_1.address),
+        types.ascii("STX-A"),
+        types.ascii("STX"),
+        types.principal("STSTW15D618BSZQB85R058DS46THH86YQQY6XCB7.stx-reserve"),
+        types.principal(
+          "STSTW15D618BSZQB85R058DS46THH86YQQY6XCB7.arkadiko-token",
+        ),
+      ], wallet_1.address)
+    ]);
+
+    let call = await chain.callReadOnlyFn("stx-reserve", "get-tokens-to-stack", [], deployer.address);
+    call.result.expectOk().expectUint(1500000000); // 1500 STX
+    block = chain.mineBlock([
+      Tx.contractCall("stacker", "initiate-stacking", [
+        types.tuple({ 'version': '0x00', 'hashbytes': '0xf632e6f9d29bfb07bc8948ca6e0dd09358f003ac'}),
+        types.uint(1), // start block height
+        types.uint(1) // 1 cycle lock period
+      ], deployer.address)
+    ]);
+    block.receipts[0].result.expectOk().expectUint(1500000000);
+
+    call = await chain.callReadOnlyFn("stacker", "get-stx-balance", [], deployer.address);
+    call.result.expectOk().expectUint(1500000000);
+
+    chain.mineEmptyBlock(300);
+
+    // now imagine we receive 450 STX for stacking
+    // and then payout vault 1 (which was the only stacker)
+    block = chain.mineBlock([
+      Tx.contractCall("stacker", "set-stacking-stx-received", [
+        types.uint(450000000),
+      ], deployer.address),
+      Tx.contractCall("stacker", "payout", [
+        types.uint(1)
+      ], deployer.address),
+      Tx.contractCall("stacker", "payout", [
+        types.uint(2)
+      ], deployer.address)
+    ]);
+    console.log(block);
+
+    call = await chain.callReadOnlyFn("vault-data", "get-vault-by-id", [types.uint(1)], deployer.address);
+    let vault = call.result.expectTuple();
+    vault['stacked-tokens'].expectUint(1299970000);
+    vault['collateral'].expectUint(1299970000);
+
+    call = await chain.callReadOnlyFn("vault-data", "get-vault-by-id", [types.uint(2)], deployer.address);
+    vault = call.result.expectTuple();
+    vault['stacked-tokens'].expectUint(649985000);
+    vault['collateral'].expectUint(649985000);
   }
 });
